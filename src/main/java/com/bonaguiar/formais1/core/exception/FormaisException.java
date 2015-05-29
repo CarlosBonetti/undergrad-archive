be6e464617b1e21c57828de2441940e9a2b6fd86
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-05-29 17:12:57 -0300

    Add serialVersionUID em FormaisException

--- src/main/java/com/bonaguiar/formais1/core/exception/FormaisException.java
+++ src/main/java/com/bonaguiar/formais1/core/exception/FormaisException.java
@@ -1,6 +1,7 @@
 package com.bonaguiar.formais1.core.exception;
 
 public class FormaisException extends Exception {
+	private static final long serialVersionUID = -5545831048247262436L;
 
 	public FormaisException(String message) {
 		super(message);
