7e3aace1618f8eeb6596c3170befcea04c9dc3b4
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-05-28 21:12:52 -0300

    Adicionada documentação à Alfabeto

--- src/main/java/com/bonaguiar/formais1/core/Alfabeto.java
+++ src/main/java/com/bonaguiar/formais1/core/Alfabeto.java
@@ -9,18 +9,31 @@ import java.util.HashSet;
 public class Alfabeto extends HashSet<Character> {
 	private static final long serialVersionUID = -2777252438918979238L;
 
+	/**
+	 * Lista de caracteres do alfabeto
+	 * @param caracteres
+	 */
 	public Alfabeto(Character[] caracteres) {
 		for(char c : caracteres) {
 			super.add(c);
 		}
 	}
 
+	/**
+	 * Lista de caracteres do alfabeto
+	 * @param caracteres
+	 */
 	public Alfabeto(char... caracteres) {
 		for(char c : caracteres) {
 			super.add(c);
 		}
 	}
 	
+	/**
+	 * String contendo os caracteres do alfabeto. Cada caracter da string é considerado como um símbolo
+	 * novo do alfabeto
+	 * @param caracteres
+	 */
 	public Alfabeto(String caracteres) {
 		this(caracteres.toCharArray());
 	}
