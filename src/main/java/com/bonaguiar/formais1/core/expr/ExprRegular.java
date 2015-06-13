30b7ca4661c1477d8477a116868d97d8399878f0
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-06-13 20:41:47 -0300

    Criada classe ExprRegular

---
+++ src/main/java/com/bonaguiar/formais1/core/expr/ExprRegular.java
@@ -0,0 +1,34 @@
+package com.bonaguiar.formais1.core.expr;
+
+import com.bonaguiar.formais1.core.exception.FormaisException;
+
+import lombok.Getter;
+
+/**
+ * Expressão regular 
+ */
+public class ExprRegular {
+	
+	/**
+	 * Expressão propriamente dita 
+	 */
+	@Getter
+	public final String expr;
+	
+	/**
+	 * Árvore sintática da expressão regular, gerada no momento de construção
+	 */
+	@Getter
+	public final Nodo<Character> tree;
+	
+	/**
+	 * Cria uma nova expressão regular
+	 * @param expr
+	 * @throws FormaisException 
+	 */
+	public ExprRegular(String expr) throws FormaisException {
+		this.expr = expr;
+		this.tree = ERParser.parse(this.expr);
+	}
+	
+}
---
+++ src/test/java/com/bonaguiar/formais1/core/expr/ExprRegularTest.java
@@ -0,0 +1,32 @@
+package com.bonaguiar.formais1.core.expr;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bonaguiar.formais1.core.exception.FormaisException;
+
+public class ExprRegularTest {
+
+	@Test
+	public void testarCriacao() throws FormaisException {
+		ExprRegular expr1 = new ExprRegular("(ab)*");
+		assertEquals("(ab)*", expr1.getExpr());
+	}
+	
+	@Test
+	public void testarArvoreSintatica() throws FormaisException {
+		ExprRegular expr1 = new ExprRegular("(ab)*(cc)*");
+		assertEquals(new Character('.'), expr1.getTree().getConteudo());
+		assertEquals(new Character('*'), expr1.getTree().getEsq().getConteudo());
+		assertEquals(new Character('*'), expr1.getTree().getDir().getConteudo());
+		assertEquals(new Character('.'), expr1.getTree().getEsq().getEsq().getConteudo());
+		assertEquals(null, expr1.getTree().getEsq().getDir());
+		assertEquals(null, expr1.getTree().getDir().getDir());		
+		assertEquals(new Character('a'), expr1.getTree().getEsq().getEsq().getEsq().getConteudo());
+		assertEquals(new Character('b'), expr1.getTree().getEsq().getEsq().getDir().getConteudo());
+		assertEquals(new Character('c'), expr1.getTree().getDir().getEsq().getEsq().getConteudo());
+		assertEquals(new Character('c'), expr1.getTree().getDir().getEsq().getDir().getConteudo());		
+	}
+
+}
