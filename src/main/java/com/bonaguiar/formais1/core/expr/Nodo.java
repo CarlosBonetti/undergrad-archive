9603ca4f8c59a5e23da3424dce69c2beca44a405
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-06-13 21:06:46 -0300

    Nodo modificado

--- src/main/java/com/bonaguiar/formais1/core/expr/Nodo.java
+++ src/main/java/com/bonaguiar/formais1/core/expr/Nodo.java
@@ -1,4 +1,4 @@
-package com.bonaguiar.formais1.core;
+package com.bonaguiar.formais1.core.expr;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +12,8 @@ import lombok.Setter;
 @Getter
 public class Nodo<T> {
 	private T conteudo;
-	private Nodo<T> left;
-	private Nodo<T> right;
+	private Nodo<T> esq;
+	private Nodo<T> dir;
 	
 	public Nodo(T conteudo) {
 		this.conteudo = conteudo;
@@ -22,44 +22,22 @@ public class Nodo<T> {
 	/**
 	 * Checa se o nodo tem um filho à esquerda
 	 */
-	public Boolean hasLeftChild() {
-		return this.left != null;
+	public Boolean temEsq() {
+		return this.esq != null;
 	}
 	
 	/**
 	 * Checa se o nodo tem um filho à direita
 	 */
-	public Boolean hasRightChild() {
-		return this.right != null;
-	}
-	
-	/**
-	 * Remove o filho à esquerda
-	 */
-	public void clearLeft() {
-		this.left = null;
-	}
-	
-	/**
-	 * Remove o filho à direita
-	 */
-	public void clearRight() {
-		this.right = null;
-	}
-	
-	/**
-	 * Remove ambos os filhos do nodo
-	 */
-	public void clear() {
-		this.clearLeft();
-		this.clearRight();
-	}
-	
+	public Boolean temDir() {
+		return this.dir != null;
+	}	
+
 	/**
 	 * Checa se o nodo é folha (não possui filhos)
 	 */
-	public Boolean isLeaf() {
-		return !this.hasLeftChild() && !this.hasRightChild();
+	public Boolean ehFolha() {
+		return !this.temEsq() && !this.temDir();
 	}
 	
 }
--- src/test/java/com/bonaguiar/formais1/core/expr/NodoTest.java
+++ src/test/java/com/bonaguiar/formais1/core/expr/NodoTest.java
@@ -1,8 +1,10 @@
-package com.bonaguiar.formais1.core;
+package com.bonaguiar.formais1.core.expr;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import com.bonaguiar.formais1.core.expr.Nodo;
 
 public class NodoTest {
 
@@ -13,71 +15,39 @@ public class NodoTest {
 	}
 	
 	@Test
-	public void testHasLeftChild() {
+	public void testarTemEsq() {
 		Nodo<Integer> node = new Nodo<Integer>(2);
 		Nodo<Integer> child = new Nodo<Integer>(-1);
-		assertEquals(false, node.hasLeftChild());
-		node.setLeft(child);
-		assertEquals(true, node.hasLeftChild());
+		assertEquals(false, node.temEsq());
+		node.setEsq(child);
+		assertEquals(true, node.temEsq());
 	}
 	
 	@Test
 	public void testHasRightChild() {
 		Nodo<Integer> node = new Nodo<Integer>(2);
 		Nodo<Integer> child = new Nodo<Integer>(-1);
-		assertEquals(false, node.hasRightChild());
-		node.setRight(child);
-		assertEquals(true, node.hasRightChild());
+		assertEquals(false, node.temDir());
+		node.setDir(child);
+		assertEquals(true, node.temDir());
 	}
 	
-	@Test
-	public void testClearLeft() {
-		Nodo<Integer> node = new Nodo<Integer>(2);
-		Nodo<Integer> child = new Nodo<Integer>(-1);
-		node.setLeft(child);
-		assertEquals(child, node.getLeft());
-		node.clearLeft();
-		assertEquals(null, node.getLeft());
-	}
-	
-	@Test
-	public void testClearRight() {
-		Nodo<Integer> node = new Nodo<Integer>(2);
-		Nodo<Integer> child = new Nodo<Integer>(-1);
-		node.setRight(child);
-		assertEquals(child, node.getRight());
-		node.clearRight();
-		assertEquals(null, node.getRight());
-	}
-	
-	@Test
-	public void testClear() {
-		Nodo<Integer> node = new Nodo<Integer>(2);
-		Nodo<Integer> child1 = new Nodo<Integer>(-1);
-		Nodo<Integer> child2 = new Nodo<Integer>(1);
-		
-		node.setRight(child1);
-		node.setLeft(child2);
-		
-		node.clear();
-		assertEquals(null, node.getRight());
-		assertEquals(null, node.getLeft());		
-	} 
-
 	@Test
 	public void testIsLeaf() {
 		Nodo<Double> node = new Nodo<Double>(2.0);
-		assertEquals(node.isLeaf(), true);		
+		assertEquals(node.ehFolha(), true);		
 		
 		Nodo<Double> child = new Nodo<Double>(-1.0);		
-		node.setLeft(child);
-		assertEquals(node.isLeaf(), false);
+		node.setEsq(child);
+		assertEquals(node.ehFolha(), false);
 		
-		node.clear();
-		assertEquals(node.isLeaf(), true);
-		node.setRight(child);
-		assertEquals(node.isLeaf(), false);
-		node.setLeft(child);
-		assertEquals(node.isLeaf(), false);
+		node.setEsq(null);
+		node.setDir(null);		
+		
+		assertEquals(node.ehFolha(), true);
+		node.setDir(child);
+		assertEquals(node.ehFolha(), false);
+		node.setEsq(child);
+		assertEquals(node.ehFolha(), false);
 	}
 }
