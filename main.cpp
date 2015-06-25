a7cdcadd1251fc881a65ef42d2fe926f99637f6a
felipepiovezan <piovezan.fpi@gmail.com>
2015-06-25 13:34:57 -0300

    build an arm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glut.h>
+#include "body.h"
 
 #define ESCAPE 27
 
@@ -44,77 +45,10 @@ void redraw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Limpe a tela e o buffer
 	//glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE); // Indique que os dois lados de qualquer superfície devem ser representados.
 	glLoadIdentity(); // Resete a corrente Modelview Matrix
-
-	glTranslatef(-1.5f,0.0f,-6.0f);
-	glRotatef(rtri,0.0f,1.0f,0.0f);
-
-	glBegin(GL_TRIANGLES); // início triângulo
-		glColor3f(1.0f,0.0f,0.0f);          // Red
-		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Front)
-		glColor3f(0.0f,1.0f,0.0f);          // Green
-		glVertex3f(-1.0f,-1.0f, 1.0f);          // Left Of Triangle (Front)
-		glColor3f(0.0f,0.0f,1.0f);          // Blue
-		glVertex3f( 1.0f,-1.0f, 1.0f);          // Right Of Triangle (Front)
-		glColor3f(1.0f,0.0f,0.0f);          // Red
-		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Right)
-		glColor3f(0.0f,0.0f,1.0f);          // Blue
-		glVertex3f( 1.0f,-1.0f, 1.0f);          // Left Of Triangle (Right)
-		glColor3f(0.0f,1.0f,0.0f);          // Green
-		glVertex3f( 1.0f,-1.0f, -1.0f);         // Right Of Triangle (Right)
-		glColor3f(1.0f,0.0f,0.0f);          // Red
-		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Back)
-		glColor3f(0.0f,1.0f,0.0f);          // Green
-		glVertex3f( 1.0f,-1.0f, -1.0f);         // Left Of Triangle (Back)
-		glColor3f(0.0f,0.0f,1.0f);          // Blue
-		glVertex3f(-1.0f,-1.0f, -1.0f);         // Right Of Triangle (Back)
-		glColor3f(1.0f,0.0f,0.0f);          // Red
-		glVertex3f( 0.0f, 1.0f, 0.0f);          // Top Of Triangle (Left)
-		glColor3f(0.0f,0.0f,1.0f);          // Blue
-		glVertex3f(-1.0f,-1.0f,-1.0f);          // Left Of Triangle (Left)
-		glColor3f(0.0f,1.0f,0.0f);          // Green
-		glVertex3f(-1.0f,-1.0f, 1.0f);          // Right Of Triangle (Left)
-	glEnd();                        // Done Drawing The Pyramid
-
-	glLoadIdentity();
-	glTranslatef(1.5f,0.0f,-6.0f);
-	glRotatef(rquad,1.0f,0.0f,0.0f);
-	glColor3f(0.5f,0.5f,1.0f);
-	glBegin(GL_QUADS); // Draw A Quad
-		glColor3f(0.0f,1.0f,0.0f);          // Set The Color To Green
-		glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Top)
-		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Top)
-		glVertex3f(-1.0f, 1.0f, 1.0f);          // Bottom Left Of The Quad (Top)
-		glVertex3f( 1.0f, 1.0f, 1.0f);          // Bottom Right Of The Quad (Top)
-		glColor3f(1.0f,0.5f,0.0f);          // Set The Color To Orange
-		glVertex3f( 1.0f,-1.0f, 1.0f);          // Top Right Of The Quad (Bottom)
-		glVertex3f(-1.0f,-1.0f, 1.0f);          // Top Left Of The Quad (Bottom)
-		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Bottom)
-		glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Bottom)
-		glColor3f(1.0f,0.0f,0.0f);          // Set The Color To Red
-		glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Front)
-		glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Front)
-		glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Front)
-		glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Front)
-		glColor3f(1.0f,1.0f,0.0f);          // Set The Color To Yellow
-		glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Back)
-		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Back)
-		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Back)
-		glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Back)
-		glColor3f(0.0f,0.0f,1.0f);          // Set The Color To Blue
-		glVertex3f(-1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Left)
-		glVertex3f(-1.0f, 1.0f,-1.0f);          // Top Left Of The Quad (Left)
-		glVertex3f(-1.0f,-1.0f,-1.0f);          // Bottom Left Of The Quad (Left)
-		glVertex3f(-1.0f,-1.0f, 1.0f);          // Bottom Right Of The Quad (Left)
-		glColor3f(1.0f,0.0f,1.0f);          // Set The Color To Violet
-		glVertex3f( 1.0f, 1.0f,-1.0f);          // Top Right Of The Quad (Right)
-		glVertex3f( 1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Right)
-		glVertex3f( 1.0f,-1.0f, 1.0f);          // Bottom Left Of The Quad (Right)
-		glVertex3f( 1.0f,-1.0f,-1.0f);          // Bottom Right Of The Quad (Right)
-	glEnd(); // Done Drawing The Quad
-
 	rtri+=0.4f;
-	rquad-=0.3f;
-
+	glTranslatef(1.5f,0.0f,-6.0f);
+	glRotatef(rtri,0.0f,1.0f,1.0f);
+	Forearm().draw();
 	glutSwapBuffers();
 }
 
@@ -151,7 +85,7 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	initGL();
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(0, 0);
 	window = glutCreateWindow("Testing OpenGL");
 	glutReshapeFunc(windowResize);
---
+++ makefile
@@ -0,0 +1,5 @@
+
+
+all:
+	g++ main.cpp -lGL -lGLU -lglut -std=c++11
+	./a.out
