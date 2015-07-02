0436abad0b38a66f7fb3dcd215d9c08be1a6b6b2
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-07-02 20:22:11 -0300

    ERParser aceitando caractere '+'

--- src/main/java/com/bonaguiar/formais1/core/expr/ERParser.java
+++ src/main/java/com/bonaguiar/formais1/core/expr/ERParser.java
@@ -16,34 +16,38 @@ public class ERParser {
 	 * Conjunto de caracteres válidos (que podem aparecer na expressão regular)
 	 */
 	public static final Set<Character> CARACTERES = new HashSet<Character>(Arrays.asList(
-		'&',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-	));
-	
+			'&',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+			));
+
 	/**
 	 * Retorna uma árvore binária que representa a expressão regular informada
+	 *
 	 * @param expr
 	 * @return
-	 * @throws FormaisException 
+	 * @throws FormaisException
 	 */
 	public static Nodo<Character> parse(String expr) throws FormaisException {
 		if (expr == null || expr.isEmpty()) {
 			return new Nodo<Character>(null);
 		}
-		
+
 		if (expr.length() == 1) {
 			return parseChar(expr.charAt(0));
 		}
-		
+
+		// Traduz símbolos especiais (como o '+') para outros mais primitivos
+		expr = traduzirSimbolosEspeciais(expr);
+
 		// Se expressão for da forma "(...)", retorna o parse da expressão interna
 		if (expr.startsWith("(") && expr.endsWith(")")) {
 			return parse(expr.substring(1, expr.length() - 1));
 		}
-		
+
 		// Procuramos pela operação de mais alto nível, seguindo a precedência dos operadores
 		int pos;
-		
+
 		// Se possuir '|'
 		pos = posOperador(expr, '|');
 		if (pos != -1) {
@@ -52,7 +56,7 @@ public class ERParser {
 			nodo.setDir(parse(expr.substring(pos + 1)));
 			return nodo;
 		}
-		
+
 		// Se possuir concatenação
 		pos = posConcat(expr);
 		if (pos != 0) {
@@ -61,33 +65,34 @@ public class ERParser {
 			nodo.setDir(parse(expr.substring(pos)));
 			return nodo;
 		}
-		
+
 		if (expr.endsWith("*")) {
 			Nodo<Character> nodo = new Nodo<Character>('*');
 			nodo.setEsq(parse(expr.substring(0, expr.length() - 1)));
 			return nodo;
 		}
-		
+
 		if (expr.endsWith("?")) {
 			Nodo<Character> nodo = new Nodo<Character>('?');
 			nodo.setEsq(parse(expr.substring(0, expr.length() - 1)));
 			return nodo;
 		}
-		
+
 		throw new FormaisException("Expressão regular má formada: '" + expr + "'");
 	}
-	
+
 	public static Nodo<Character> parseChar(char c) throws FormaisException {
 		if (!CARACTERES.contains(c)) {
 			throw new FormaisException("Caractere '" + c + "' não é válido como alfabeto para expressões regulares");
 		}
 		return new Nodo<Character>(c);
 	}
-	
+
 	/**
 	 * Retorna em qual posição da expressão encontra-se o operador especificado, sem "entrar" em parênteses
 	 * Ou seja, sub-expressões dentro de parênteses não são consideradas pela busca
 	 * Retorna -1 caso o caracter não tenha sido encontrado
+	 *
 	 * @param expr
 	 * @param op
 	 * @return A primeira posição encontrada do operador op ou -1 caso não encontrado
@@ -105,33 +110,71 @@ public class ERParser {
 			}
 			i++;
 		}
-		
+
 		return -1;
 	}
-	
+
 	/**
 	 * Retorna a posição na qual a expressão pode ser divida seguindo o operador de concatenação
 	 * Exemplo: "(a|c)*bc" -> 6. Indicando que 0-5 representa a primeira parte da concatenação "(a|c)*" e 6-end
 	 * representa a segunda parte da concatenação "bc"
+	 *
 	 * @param expr
 	 * @return A posição da divisão ou 0 se não existe concatenação na expressão
 	 */
 	public static int posConcat(String expr) {
 		int nivel = 0; // Nível atual de parêntese
 		char[] arr = expr.toCharArray();
-		
+
 		for (int i = 0; i < expr.length() - 1; i++) {
 			if (arr[i] == '(') {
 				nivel++;
 			} else if (arr[i] == ')') {
 				nivel--;
-			} 
-			
-			if (nivel == 0 && arr[i+1] != '*' && arr[i+1] != '?') {
+			}
+
+			if (nivel == 0 && arr[i + 1] != '*' && arr[i + 1] != '?') {
 				return i + 1;
 			}
 		}
-		
+
 		return 0;
+	}
+
+	/**
+	 * Transforma a expressão com símbolos especiais para uma expressão equivalente com símbolos primitivos
+	 * Exemplo: 'a+' = 'aa*'
+	 *
+	 * @param expr
+	 * @return
+	 */
+	public static String traduzirSimbolosEspeciais(String expr) {
+		while (expr.indexOf("+") != -1) {
+			int pos = expr.indexOf("+");
+
+			String duplicate = expr.substring(pos - 1, pos);
+
+			// Para expressões regulares do tipo '(ab)+', devemos duplicar todo o conteúdo dentro do parêntese
+			if (duplicate.equals(")")) {
+				int depth = 0; // nível atual de parênteses
+				for (int i = pos - 1; i >= 0; i--) {
+					Character c = expr.charAt(i);
+					if (c.equals(')')) {
+						depth++;
+					} else if (c.equals('(')) {
+						depth--;
+					}
+
+					if (depth == 0) {
+						duplicate = expr.substring(i, pos);
+						break;
+					}
+				}
+			}
+
+			expr = expr.replaceFirst("\\+", duplicate + "*");
+		}
+
+		return expr;
 	}
 }
--- src/test/java/com/bonaguiar/formais1/core/expr/ERParserTest.java
+++ src/test/java/com/bonaguiar/formais1/core/expr/ERParserTest.java
@@ -1,6 +1,7 @@
 package com.bonaguiar.formais1.core.expr;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -12,24 +13,24 @@ public class ERParserTest {
 	public void testarParseChar() throws FormaisException {
 		Nodo<Character> nodo = ERParser.parseChar('a');
 		assertEquals(new Character('a'), nodo.getConteudo());
-		
+
 		Nodo<Character> nodo2 = ERParser.parseChar('&');
 		assertEquals(new Character('&'), nodo2.getConteudo());
-		
+
 		Nodo<Character> nodo3 = ERParser.parseChar('0');
 		assertEquals(new Character('0'), nodo3.getConteudo());
 	}
-	
-	@Test(expected=FormaisException.class)
+
+	@Test(expected = FormaisException.class)
 	public void testarParseCharInvalido1() throws FormaisException {
-		 ERParser.parseChar('*');
+		ERParser.parseChar('*');
 	}
-	
-	@Test(expected=FormaisException.class)
+
+	@Test(expected = FormaisException.class)
 	public void testarParseCharInvalido2() throws FormaisException {
-		 ERParser.parseChar('>');
+		ERParser.parseChar('>');
 	}
-	
+
 	@Test
 	public void testarPos() {
 		assertEquals(2, ERParser.posOperador("ab|dc", '|'));
@@ -37,7 +38,7 @@ public class ERParserTest {
 		assertEquals(-1, ERParser.posOperador("(ab|dc)", '|'));
 		assertEquals(14, ERParser.posOperador("((ab|dc)de?|r)|p", '|'));
 	}
-	
+
 	@Test
 	public void testarPosConcat() {
 		assertEquals(1, ERParser.posConcat("ab"));
@@ -49,54 +50,54 @@ public class ERParserTest {
 		assertEquals(6, ERParser.posConcat("(a|c)*bc"));
 		assertEquals(0, ERParser.posConcat("a*"));
 	}
-	
+
 	@Test
 	public void testarParse() throws FormaisException {
 		Nodo<Character> nodo = ERParser.parse("");
 		assertEquals(null, nodo.getConteudo());
-		
+
 		nodo = ERParser.parse("(a)");
 		assertEquals(new Character('a'), nodo.getConteudo());
 		assertTrue(nodo.ehFolha());
-		
+
 		nodo = ERParser.parse("((((b))))");
 		assertEquals(new Character('b'), nodo.getConteudo());
 		assertTrue(nodo.ehFolha());
-		
+
 		nodo = ERParser.parse("a|b");
 		assertEquals(new Character('|'), nodo.getConteudo());
 		assertEquals(new Character('a'), nodo.getEsq().getConteudo());
 		assertEquals(new Character('b'), nodo.getDir().getConteudo());
-		
+
 		nodo = ERParser.parse("xy");
 		assertEquals(new Character('.'), nodo.getConteudo());
 		assertEquals(new Character('x'), nodo.getEsq().getConteudo());
 		assertEquals(new Character('y'), nodo.getDir().getConteudo());
-		
+
 		nodo = ERParser.parse("a(bc)");
 		assertEquals(new Character('.'), nodo.getConteudo());
 		assertEquals(new Character('a'), nodo.getEsq().getConteudo());
 		assertEquals(new Character('.'), nodo.getDir().getConteudo());
 		assertEquals(new Character('b'), nodo.getDir().getEsq().getConteudo());
 		assertEquals(new Character('c'), nodo.getDir().getDir().getConteudo());
-		
+
 		nodo = ERParser.parse("a*");
 		assertEquals(new Character('*'), nodo.getConteudo());
 		assertEquals(new Character('a'), nodo.getEsq().getConteudo());
 		assertEquals(null, nodo.getDir());
-		
+
 		nodo = ERParser.parse("b?");
 		assertEquals(new Character('?'), nodo.getConteudo());
 		assertEquals(new Character('b'), nodo.getEsq().getConteudo());
 		assertEquals(null, nodo.getDir());
-		
+
 		nodo = ERParser.parse("(ab)*");
 		assertEquals(new Character('*'), nodo.getConteudo());
 		assertEquals(new Character('.'), nodo.getEsq().getConteudo());
 		assertEquals(null, nodo.getDir());
 		assertEquals(new Character('a'), nodo.getEsq().getEsq().getConteudo());
 		assertEquals(new Character('b'), nodo.getEsq().getDir().getConteudo());
-		
+
 		nodo = ERParser.parse("((a|c))*bc?(a|b|c)*");
 		assertEquals(new Character('.'), nodo.getConteudo());
 		assertEquals(new Character('*'), nodo.getEsq().getConteudo());
@@ -111,19 +112,38 @@ public class ERParserTest {
 		assertEquals(new Character('*'), nodo.getDir().getDir().getDir().getConteudo());
 		assertEquals(new Character('|'), nodo.getDir().getDir().getDir().getEsq().getConteudo());
 		assertEquals(new Character('a'), nodo.getDir().getDir().getDir().getEsq().getEsq().getConteudo());
-		assertEquals(new Character('|'), nodo.getDir().getDir().getDir().getEsq().getDir().getConteudo());	
+		assertEquals(new Character('|'), nodo.getDir().getDir().getDir().getEsq().getDir().getConteudo());
 		assertEquals(new Character('b'), nodo.getDir().getDir().getDir().getEsq().getDir().getEsq().getConteudo());
 		assertEquals(new Character('c'), nodo.getDir().getDir().getDir().getEsq().getDir().getDir().getConteudo());
+
+		nodo = ERParser.parse("a+"); // traduz-se para 'aa*'
+		assertEquals(new Character('.'), nodo.getConteudo());
+		assertEquals(new Character('a'), nodo.getEsq().getConteudo());
+		assertEquals(new Character('*'), nodo.getDir().getConteudo());
+		assertEquals(new Character('a'), nodo.getDir().getEsq().getConteudo());
+
+		nodo = ERParser.parse("(ab*)+"); // traduz-se para '(ab*)(ab*)*'
+		assertEquals(new Character('.'), nodo.getConteudo());
+		assertEquals(new Character('.'), nodo.getEsq().getConteudo());
+		assertEquals(new Character('*'), nodo.getDir().getConteudo());
+		assertEquals(new Character('.'), nodo.getDir().getEsq().getConteudo());
 	}
 
-	@Test(expected=FormaisException.class)
+	@Test(expected = FormaisException.class)
 	public void testarParseDeExprInvalidas1() throws FormaisException {
 		ERParser.parse("*");
 	}
-	
-	@Test(expected=FormaisException.class)
+
+	@Test(expected = FormaisException.class)
 	public void testarParseDeExprInvalidas2() throws FormaisException {
 		ERParser.parse("abc ");
 	}
-	
+
+	@Test
+	public void testarTraduzirSimbolosEspeciais_Mais() {
+		assertEquals("aa*", ERParser.traduzirSimbolosEspeciais("a+"));
+		assertEquals("a*(bc?abb*)", ERParser.traduzirSimbolosEspeciais("a*(bc?ab+)"));
+		assertEquals("a(bc?)(bc?)*", ERParser.traduzirSimbolosEspeciais("a(bc?)+"));
+		assertEquals("a(ed(kl*|bc)(a)*)(ed(kl*|bc)(a)*)*", ERParser.traduzirSimbolosEspeciais("a(ed(kl*|bc)(a)*)+"));
+	}
 }
