222b724bdd47089e503db07cf172646863cf71e5
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-07-03 18:32:34 -0300

    Added light and depth test corrected

--- main.cpp
+++ main.cpp
@@ -11,16 +11,29 @@ float rtri = 90;
 int time = 0;
 int KEY_STATES[256];
 
+void init_light() {
+	// Let there be light
+	GLfloat light_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat light_position[]= { 0.0f, 0.0f, 20.0f, 1.0f };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	//glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+}
+
 void initGL() {
 	glShadeModel(GL_SMOOTH); // Habilita sombreamento suavizado
 	// Mistura cores de forma suave me um polígono
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Fundo negro
 	glClearDepth(1.0f); // Inicialização do Depth Buffer
-	glEnable(GL_DEPTH_TEST); // Habilita teste de profundidade
 	glDepthFunc(GL_LEQUAL); // Define qual teste de profundidade vai ser feito
+	glDepthMask(true);
 
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // Calculos de perspectiva legaizinhos
+	init_light();
 }
 
 void windowResize(int width, int height) {
@@ -28,10 +41,11 @@ void windowResize(int width, int height) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	if (height==0 )  // Calculate The Aspect Ratio Of The Window
-		gluPerspective ( 80, ( float ) width, 1.0, 5000.0 );
-	else
-		gluPerspective ( 80, ( float ) width / ( float ) height, 1.0, 5000.0 );
+	if (height == 0)
+		height = 1;
+
+	// Calculate The Aspect Ratio Of The Window
+	gluPerspective(80, (GLfloat) width / (GLfloat) height, 1.0f, 500.0);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -40,14 +54,17 @@ void windowResize(int width, int height) {
 void redraw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Limpe a tela e o buffer
 	//glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE); // Indique que os dois lados de qualquer superfície devem ser representados.
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
 	glLoadIdentity(); // Resete a corrente Modelview Matrix
 
-//	rtri+=0.7f;
+	rtri+=0.7f;
 	glTranslatef(0.0f, 3.0f, -23.0f);
 	glRotatef(rtri,0.0f,1.0f,0);
-
 	Person().draw(time);
-
 	glutSwapBuffers();
 }
 
