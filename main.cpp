927a79f6525f668195f2cf8228ce3b3106631334
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-07-04 17:40:53 -0300

    Change camera movement. Add ambient light and fix for light + colors

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glut.h>
+#include <math.h>
 
 #include "person.h"
 
 #define ESCAPE 27
 
 int window;
-float rtri = 90;
+float rotation = 90; // Camera rotation
 int time = 0;
 int KEY_STATES[256];
 
@@ -15,12 +16,25 @@ void init_light() {
 	// Let there be light
 	GLfloat light_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
 	GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat light_position[]= { 0.0f, 0.0f, 20.0f, 1.0f };
+	GLfloat light_position[]= { -30.0f, 30.0f, -20.0f, 1.0f };
+	glPushMatrix();
+		// Draw the light position just for debugging purposes...
+		glTranslatef(light_position[0], light_position[1], light_position[2]);
+		glutSolidCube(1);
+	glPopMatrix();
 
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 	//glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+	// Global ambient light
+	GLfloat lmodel_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL); // Fiz para a cor funcionar junto com luz
 }
 
 void initGL() {
@@ -56,24 +70,34 @@ void redraw() {
 	//glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE); // Indique que os dois lados de qualquer superfície devem ser representados.
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
 
+	init_light();
 	glLoadIdentity(); // Resete a corrente Modelview Matrix
 
-	rtri+=0.7f;
 	glTranslatef(0.0f, 3.0f, -23.0f);
-	glRotatef(rtri,0.0f,1.0f,0);
-	Person().draw(time);
+
+	//gluLookAt(	sinf(rotation), 0, cosf(rotation),
+	//			0, 0, 0,
+	//			0, 1, 0);
+
+	glPushMatrix();
+		glRotatef(rotation, 0, 1, 0);
+		Person().draw(time);
+	glPopMatrix();
 	glutSwapBuffers();
 }
 
 void handle_keys() {
-	if (KEY_STATES[GLUT_KEY_RIGHT]) {
+	if (KEY_STATES[GLUT_KEY_UP]) {
 		time++;
-	}
-    if (KEY_STATES[GLUT_KEY_LEFT]) {
+	} else if (KEY_STATES[GLUT_KEY_DOWN]) {
 		time--;
+	}
+
+	if (KEY_STATES[GLUT_KEY_RIGHT]) {
+		rotation -= 2.0f;
+	} else if (KEY_STATES[GLUT_KEY_LEFT]) {
+		rotation += 2.0f;
 	}
 }
 
@@ -96,10 +120,10 @@ void key_pressed(unsigned char key, int x, int y) {
 void key_special_down(int key, int x, int y) {
 	KEY_STATES[key] = true;
 	switch(key) {
-		case GLUT_KEY_UP:
+		case GLUT_KEY_F1:
 			glutFullScreen();
 			break;
-		case GLUT_KEY_DOWN:
+		case GLUT_KEY_F2:
 			glutReshapeWindow(640, 480);
 			break;
 	}
