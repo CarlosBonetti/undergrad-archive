e557be6384c75a7eb67a2b374f5bca405cfe1636
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-07-04 17:04:40 -0300

    draw_box changed to use SolidCube

--- utils.h
+++ utils.h
@@ -3,46 +3,19 @@
 
 #include <GL/glut.h>
 #include "movement.h"
+
 void apply_state(const movement::state& s){
     glRotated(s.rx,1,0,0);
     glRotated(s.ry,0,1,0);
     glRotated(s.rz,0,0,1);
 }
 
-
-void draw_box(double width, double height, double depth) {
-	double _width = width/2,
-		   _height = height/2,
-		   _depth = depth;
-
-	glBegin(GL_QUADS); // Draw A Quad
-		glVertex3f(-_width, _height, 0.0f);
-		glVertex3f( _width, _height, 0.0f);
-		glVertex3f( _width,-_height, 0.0f);
-		glVertex3f(-_width,-_height, 0.0f);
-
-		glVertex3f(-_width, _height, _depth);
-		glVertex3f( _width, _height, _depth);
-		glVertex3f( _width,-_height, _depth);
-		glVertex3f(-_width,-_height, _depth);
-
-		glVertex3f(-_width, _height, 0.0f);
-		glVertex3f(-_width,-_height, 0.0f);
-		glVertex3f(-_width,-_height, _depth);
-		glVertex3f(-_width, _height, _depth);
-
-		glVertex3f(_width, _height, 0.0f);
-		glVertex3f(_width,-_height, 0.0f);
-		glVertex3f(_width,-_height, _depth);
-		glVertex3f(_width, _height, _depth);
-
-		glVertex3f( _width,-_height, 0.0f);
-		glVertex3f(-_width,-_height, 0.0f);
-		glVertex3f(-_width,-_height, _depth);
-		glVertex3f( _width,-_height, _depth);
-	glEnd();
+void draw_box(double scaleX, double scaleY, double scaleZ) {
+	glPushMatrix();
+		glTranslatef(0, 0, scaleZ/2);
+		glScalef(scaleX, scaleY, scaleZ);
+		glutSolidCube(1);
+	glPopMatrix();
 }
-
-
 
 #endif
