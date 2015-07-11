4a9b66a1a9404c3118527f518ce7339c3ed2a733
Carlos Bonetti <carlosb_bc@hotmail.com>
2015-07-10 22:16:29 -0300

    Mais um pouco do ParserGenerator implementado :penguin:

--- src/main/java/com/bonaguiar/formais2/core/ParserGenerator.java
+++ src/main/java/com/bonaguiar/formais2/core/ParserGenerator.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bonaguiar.formais2.core.GLC.FormaSentencial;
+
 /**
  * Um gerador de parser para gramáticas livres de contexto
  * que usa a técnica Descendente Recursivo
@@ -35,7 +37,82 @@ public class ParserGenerator {
 	}
 
 	public String getMetodos() {
-		return "TODO";
+		String result = "";
+		for (String nt : this.glc.getNaoTerminais()) {
+			Template metodo = this.getMetodoTemplate();
+			metodo = metodo.replace("$(nome)", nt);
+			metodo = metodo.replace("$(corpo)", this.getCorpo(nt));
+			result += metodo.toString();
+		}
+
+		return result;
+	}
+
+	protected String getCorpo(String nt) {
+		String result = "";
+		List<FormaSentencial> producoes = this.glc.getProducoes(nt);
+
+		boolean first = true;
+		for (FormaSentencial fs : producoes) {
+			if (fs.equals(GrammarUtils.PRODUCAO_VAZIA)) {
+				continue;
+			}
+
+			if (!first) {
+				result += "else ";
+			}
+
+			result += this.getIfProducao(fs, 0);
+			first = false;
+		}
+
+		Template ultimoElseTemplate = this.getElseTemplate();
+		if (producoes.contains(GrammarUtils.PRODUCAO_VAZIA)) {
+			ultimoElseTemplate = ultimoElseTemplate.replace("$(body)", "");
+		} else {
+			String exc = "throw new ParseException(x, 0);";
+			ultimoElseTemplate = ultimoElseTemplate.replace("$(body)", exc);
+		}
+
+		result += ultimoElseTemplate.toString();
+
+		return result;
+	}
+
+	protected String getIfProducao(FormaSentencial formaSentencial, int index) {
+		Template ifTemplate = this.getIfTemplate();
+		String simboloAtual = formaSentencial.get(index);
+
+		// TODO: se for não terminal, fazer aquilo...
+		ifTemplate = ifTemplate.replace("$(condition)", "x == '" + simboloAtual + "'");
+
+		ifTemplate = ifTemplate.replace("$(body)", "TODO");
+		return ifTemplate.toString();
+	}
+
+	protected Template getIfTemplate() {
+		Template template = new Template();
+		template.add("if ($(condition)) {");
+		template.add("	$(body)");
+		template.add("}");
+		return template;
+	}
+
+	protected Template getElseTemplate() {
+		Template template = new Template();
+		template.add("else {");
+		template.add("	$(body)");
+		template.add("}");
+		return template;
+	}
+
+	protected Template getMetodoTemplate() {
+		Template template = new Template();
+		template.add("public static void $(nome)(x) {");
+		template.add("	$(corpo)");
+		template.add("}");
+		template.add("");
+		return template;
 	}
 
 	protected Template getParserTemplate() throws IOException {
@@ -57,7 +134,8 @@ public class ParserGenerator {
 		}
 
 		/**
-		 * Altera toda a sequência de caracteres encontrada pelo replacement
+		 * Altera toda a sequência de caracteres encontrada pelo replacement, mantendo
+		 * níveis de tab encontrados
 		 *
 		 * @param target
 		 * @param replacement
@@ -66,7 +144,27 @@ public class ParserGenerator {
 		public Template replace(String target, String replacement) {
 			Template novo = new Template();
 			for (String line : this) {
-				novo.add(line.replace(target, replacement));
+				String newLine = line.replace(target, replacement);
+
+				// Se a linha foi alterada, fazemos uns cambalacho pra deixar os níveis de tab
+				// corretos mesmo para replacements com multilinhas
+				if (!newLine.equals(line)) {
+					int lastTab = -1;
+					for (Character c : line.toCharArray()) {
+						if (c == '\t') {
+							lastTab++;
+						} else {
+							break;
+						}
+					}
+
+					if (lastTab != -1) {
+						String tabs = line.substring(0, lastTab + 1);
+						newLine = newLine.replace("\n", "\n" + tabs);
+					}
+				}
+
+				novo.add(newLine);
 			}
 			return novo;
 		}
@@ -75,7 +173,7 @@ public class ParserGenerator {
 		public String toString() {
 			String result = "";
 			for (String line : this) {
-				result += line + "\n";
+				result += line + System.lineSeparator();
 			}
 			return result;
 		}
--- src/test/java/com/bonaguiar/formais2/core/ParserGeneratorTest.java
+++ src/test/java/com/bonaguiar/formais2/core/ParserGeneratorTest.java
@@ -9,7 +9,7 @@ public class ParserGeneratorTest {
 
 	@Test
 	public void test() throws ParseException, IOException {
-		GLC glc = new GLC("S -> a B \n B -> b B | b");
+		GLC glc = new GLC("S -> a B \n B -> a B | b | C \n C -> c | &");
 		ParserGenerator generator = new ParserGenerator(glc);
 		System.out.println(generator.getParser());
 	}
